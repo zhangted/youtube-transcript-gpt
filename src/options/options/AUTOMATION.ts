bec02d12ffdb0c9a11dc50351e5705257cf41253
Teddy Zhang <tedcbook@gmail.com>
2023-06-16 00:38:19 -0400

    add option to turn off auto summarize

---
+++ src/options/options/AUTOMATION.ts
@@ -0,0 +1,6 @@
+export const AUTOMATION: string[] = [
+  "Yes - Summarize when video loads",
+  "No - Never summarize unless requested",
+];
+
+export const AUTOMATION_DEFAULT: string = AUTOMATION[0];
